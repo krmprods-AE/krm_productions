94707f6b485aa1d85a283ca320e68ca17f1c64c8
sotiris-st <sotiriospantelistamou@gmail.com>
2025-11-04 01:28:29 +0200

    the correct 3rd dag

--- 3d.py
+++ 3d.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="3d_dag",
+    dag_id="3d_dag_the-correct-one",
     start_date=datetime(2023, 1, 1),
     schedule="@daily",
     catchup=False,
