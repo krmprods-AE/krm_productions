40414588df09c60572408ebb36af4e4a38003d5f
sotiris-st <sotiriospantelistamou@gmail.com>
2025-11-26 22:09:04 +0200

    test minio correct s3

--- testminio.py
+++ testminio.py
@@ -5,11 +5,20 @@ from datetime import datetime
 
 def test_minio():
     hook = S3Hook(aws_conn_id="minio_s3")
-    print("Buckets:", hook.list_buckets())
-    print("Files:", hook.list_keys(bucket_name="example"))
 
-with DAG("test_minio_conn", start_date=datetime(2024,1,1), schedule=None, catchup=False):
+    # List files inside your bucket "example"
+    keys = hook.list_keys(bucket_name="example")
+
+    print("Files in bucket:", keys)
+    return keys
+
+with DAG(
+    dag_id="test_minio_conn",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False
+):
     PythonOperator(
         task_id="test_minio",
-        python_callable=test_minio
+        python_callable=test_minio,
     )
