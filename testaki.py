440f6e83753aaf2419df43f191b5d84948672701
sotiris-st <sotiriospantelistamou@gmail.com>
2025-11-29 00:01:45 +0200

    orestisok

--- testaki.py
+++ testaki.py
@@ -4,7 +4,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
 
 def test_minio():
-    hook = S3Hook(aws_conn_id="minio_s3")
+    hook = S3Hook(aws_conn_id="orestis")
 
     # List files inside your bucket "example"
     keys = hook.list_keys(bucket_name="test")
