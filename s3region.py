3447a4f65b811f61487fdda151211ba29974337f
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-12 16:09:15 +0200

    cor

--- s3region.py
+++ s3region.py
@@ -17,9 +17,19 @@ with DAG(
     name="airflow-spark-job",
     verbose=True,
     spark_binary="/opt/spark/bin/spark-submit",
-    conf={ # ✅ MinIO compatibility (region/signing)
-        "spark.hadoop.fs.s3a.endpoint.region": "us-east-1",
-        "spark.hadoop.fs.s3a.region": "us-east-1",
+    conf={
+        #"spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
+        #"spark.hadoop.fs.s3a.path.style.access": "true",
+        #"spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
+
+        "spark.hadoop.fs.s3a.access.key": "mycustomuser",
+        "spark.hadoop.fs.s3a.secret.key": "pakekfoeo3030d3*(&&&(*!",
+        #"spark.hadoop.fs.s3a.aws.credentials.provider":
+        #    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+
+        # ✅ MinIO compatibility (region/signing)
+        #"spark.hadoop.fs.s3a.endpoint.region": "us-east-1",
+        #"spark.hadoop.fs.s3a.region": "us-east-1",
     },
 )
 
