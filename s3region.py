8cf06831dd11aeeb888814a21de7789b64cf8977
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-12 14:35:18 +0200

    spark_region_s3

---
+++ s3region.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="spark-region-config",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    spark_job = SparkSubmitOperator(
+    task_id="spark-fetch-correct",
+    application="s3a://spark-jobs/job.py",
+    conn_id="spark_standalone",
+    deploy_mode="client",
+    name="airflow-spark-job",
+    verbose=True,
+    spark_binary="/opt/spark/bin/spark-submit",
+    conf={ # ✅ MinIO compatibility (region/signing)
+        "spark.hadoop.fs.s3a.endpoint.region": "us-east-1",
+        "spark.hadoop.fs.s3a.region": "us-east-1",
+    },
+)
+
