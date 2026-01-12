59594c2700537df7a712dade5712db1c70c6028e
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-12 13:33:16 +0200

    conn_id_minio

--- spark-fetch-correct.py
+++ spark-fetch-correct.py
@@ -13,8 +13,8 @@ with DAG(
     spark_job = SparkSubmitOperator(
         task_id="spark-fetch-correct",
         application="s3a://spark-jobs/job.py",  # or s3a://... if you want
-        conn_id="spark_standalone",
-        conn_id="spark_standalone",
+        conn_id="minio_s3",
+        #conn_id="spark_standalone",
         deploy_mode="client",
         name="airflow-spark-job",
         verbose=True,
