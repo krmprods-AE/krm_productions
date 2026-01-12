1e38dc90ec4f0e0333c6e3e83a5c66d098658910
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-12 18:02:31 +0200

    ssn3

--- s3region.py
+++ s3region.py
@@ -3,7 +3,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 from datetime import datetime
 
 with DAG(
-    dag_id="spark-region-config-2",
+    dag_id="spark-region-config-3",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
@@ -19,17 +19,17 @@ with DAG(
     spark_binary="/opt/spark/bin/spark-submit",
     conf={
         "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
-        "spark.hadoop.fs.s3a.path.style.access": "true",
-        "spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
+        #"spark.hadoop.fs.s3a.path.style.access": "true",
+        #"spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
 
         "spark.hadoop.fs.s3a.access.key": "mycustomuser",
         "spark.hadoop.fs.s3a.secret.key": "pakekfoeo3030d3*(&&&(*!",
-        "spark.hadoop.fs.s3a.aws.credentials.provider":
-            "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
+        #"spark.hadoop.fs.s3a.aws.credentials.provider":
+        #    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
 
         # ✅ MinIO compatibility (region/signing)
-        "spark.hadoop.fs.s3a.endpoint.region": "us-east-1",
-        "spark.hadoop.fs.s3a.region": "us-east-1",
+        #"spark.hadoop.fs.s3a.endpoint.region": "us-east-1",
+        #"spark.hadoop.fs.s3a.region": "us-east-1",
     },
 )
 
