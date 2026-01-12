900b008db226ab332318ee04ecafbbe87f23adc1
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-12 18:06:31 +0200

    last

--- s3region.py
+++ s3region.py
@@ -18,12 +18,12 @@ with DAG(
     verbose=True,
     spark_binary="/opt/spark/bin/spark-submit",
     conf={
-        "spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
+        #"spark.hadoop.fs.s3a.endpoint": "http://minio:9000",
         #"spark.hadoop.fs.s3a.path.style.access": "true",
         #"spark.hadoop.fs.s3a.connection.ssl.enabled": "false",
 
-        "spark.hadoop.fs.s3a.access.key": "mycustomuser",
-        "spark.hadoop.fs.s3a.secret.key": "pakekfoeo3030d3*(&&&(*!",
+        #"spark.hadoop.fs.s3a.access.key": "mycustomuser",
+        #"spark.hadoop.fs.s3a.secret.key": "pakekfoeo3030d3*(&&&(*!",
         #"spark.hadoop.fs.s3a.aws.credentials.provider":
         #    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider",
 
