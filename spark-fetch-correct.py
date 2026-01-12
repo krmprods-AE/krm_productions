6c1215c430dbf2151d7ec0cd194036b1328b7ba0
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-12 13:24:56 +0200

    correct_spark_submit_s3

---
+++ spark-fetch-correct.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="spark_submit_simple",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    spark_job = SparkSubmitOperator(
+        task_id="spark-fetch-correct",
+        application="s3a://spark-jobs/job.py",  # or s3a://... if you want
+        conn_id="spark_standalone",
+        deploy_mode="client",
+        name="airflow-spark-job",
+        verbose=True,
+
+        # 🔑 THIS IS THE KEY LINE
+        spark_binary="/opt/spark/bin/spark-submit",
+    )
