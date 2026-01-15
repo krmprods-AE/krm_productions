0b3e1aa56270bc77f3dd8cceac8c04d3c9d79e33
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-15 11:28:26 +0200

    httpemployee3

---
+++ http_employee3.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.utils.dates import days_ago
+import json
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+}
+
+with DAG(
+    dag_id="http_operator_employee3",
+    default_args=default_args,
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["http","spark","spark_gateway","employee3"],
+) as dag:
+
+    submit_spark_job = SimpleHttpOperator(
+        task_id="submit_spark_job",
+        http_conn_id="employee3",
+        endpoint="/submit-job",
+        method="POST",
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({
+            "jar": "job.py",        # your API still uses "jar"
+            "args": []              # optional args
+        }),
+        response_filter=lambda response: response.json(),
+        log_response=True,
+    )
