61c8b61fd518abc5e049d1a2d7ba854f03a44c02
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-17 19:20:55 +0200

    wait

--- http_employee3.py
+++ http_employee3.py
@@ -1,12 +1,31 @@
 from airflow import DAG
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.utils.dates import days_ago
+from airflow.sensors.python import PythonSensor
+import boto3
 import json
 
 default_args = {
     "owner": "airflow",
     "retries": 1,
 }
+def parquet_exists():
+    s3 = boto3.client(
+        "s3",
+        endpoint_url="http://minio:9000",
+        aws_access_key_id="mycustomuser",
+        aws_secret_access_key="pakekfoeo3030d3*(&&&(*!",
+    )
+
+    response = s3.list_objects_v2(
+        Bucket="analytics",
+        Prefix="joined_orders/"
+    )
+
+    if "Contents" not in response:
+        return False
+
+    return any(obj["Key"].endswith(".parquet") for obj in response["Contents"])
 
 with DAG(
     dag_id="http_operator_employee3_2_Different_Spark_Jobs",
@@ -14,7 +33,7 @@ with DAG(
     start_date=days_ago(1),
     schedule_interval=None,
     catchup=False,
-    tags=["http","spark","spark_gateway","employee3","S3","2_jobs"],
+    tags=["http","spark","spark_gateway","employee3","S3","2_jobs","sensor","wait"],
 ) as dag:
 
     submit_spark_job1 = SimpleHttpOperator(
@@ -44,5 +63,12 @@ with DAG(
         response_filter=lambda response: response.json(),
         log_response=True,
     )
+    wait_for_parquet = PythonSensor(
+    task_id="wait_for_parquet_files",
+    python_callable=parquet_exists,
+    poke_interval=10,
+    timeout=300,
+    mode="reschedule",
+        )
 
-    submit_spark_job1 >> submit_spark_job2
+    submit_spark_job1 >> wait_for_parquet >> submit_spark_job2
