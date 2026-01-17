010b213fefdbf9946c035724ef29abb23d18082d
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-17 18:22:41 +0200

    2_jobs

--- http_employee3.py
+++ http_employee3.py
@@ -9,15 +9,15 @@ default_args = {
 }
 
 with DAG(
-    dag_id="http_operator_employee3_S3",
+    dag_id="http_operator_employee3_2_Different_Spark_Jobs",
     default_args=default_args,
     start_date=days_ago(1),
     schedule_interval=None,
     catchup=False,
-    tags=["http","spark","spark_gateway","employee3","S3"],
+    tags=["http","spark","spark_gateway","employee3","S3","2_jobs"],
 ) as dag:
 
-    submit_spark_job = SimpleHttpOperator(
+    submit_spark_job1 = SimpleHttpOperator(
         task_id="submit_spark_job",
         http_conn_id="employee3",
         endpoint="/submit-job",
@@ -30,3 +30,19 @@ with DAG(
         response_filter=lambda response: response.json(),
         log_response=True,
     )
+
+    submit_spark_job2 = SimpleHttpOperator(
+        task_id="submit_spark_job",
+        http_conn_id="employee3",
+        endpoint="/submit-job",
+        method="POST",
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({
+            "jar": "s3a://spark-jobs/1_csv_write.py",        # your API still uses "jar"
+            "args": []              # optional args
+        }),
+        response_filter=lambda response: response.json(),
+        log_response=True,
+    )
+
+    submit_spark_job1 >> submit_spark_job2
