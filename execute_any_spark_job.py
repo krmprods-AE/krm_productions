ae8e3ab425c7412ca95dee8686d6574a7d30cfdb
sotiris-st <sotiriospantelistamou@gmail.com>
2026-01-18 23:59:19 +0200

    execute_any_spark_job

---
+++ execute_any_spark_job.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.utils.dates import days_ago
+from airflow.sensors.python import PythonSensor
+import boto3
+import json
+
+with DAG(
+    dag_id="execute_any_spark_job",
+    default_args=default_args,
+    start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+    tags=["http","spark","spark_gateway","employee3","S3","2_jobs","sensor","wait"],
+) as dag:
+
+    submit_spark_job1 = SimpleHttpOperator(
+        task_id="submit_spark_job1",
+        http_conn_id="employee3",
+        endpoint="/submit-job",
+        method="POST",
+        headers={"Content-Type": "application/json"},
+        data=json.dumps({
+            "jar": "s3a://spark-jobs/tet.py",        # your API still uses "jar"
+            "args": []              # optional args
+        }),
+        response_filter=lambda response: response.json(),
+        log_response=True,
+    )
